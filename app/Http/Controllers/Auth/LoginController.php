408f7fcea860255f42e99e0c79d153f4cfee079a
unknown <znburgos@gmail.com>
2020-01-25 20:06:52 -0600

    Final del curso

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\http\Request as IlluminateRequest;
 
 class LoginController extends Controller
 {
@@ -40,5 +41,22 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
+    }
+
+    public function showLoginForm(IlluminateRequest $request)
+    {
+        if ($request->has('redirect_to')){
+            session()->put('redirect_to', $request->input('redirect_to'));
+        }
+
+        return view('auth.login');
+    }
+
+    public function redirectTo()
+    {
+        if (session()->has('redirect_to'))
+            return session()->pull('redirect_to');
+
+        return $this->redirectTo;
     }
 }
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -34,9 +34,15 @@
             </div>
 
             <div class="text-center" data-toggle="modal" data-target="#modalAddToCart">
-                <button class="btn btn-primary btn-round">
-                    <i class="material-icons">add</i> Añadir al carrito de compras
-                </button>
+                @if (auth()->check())
+                    <button class="btn btn-primary btn-round">
+                        <i class="material-icons">add</i> Añadir al carrito de compras
+                    </button>
+                @else
+                    <a href="{{ url('/login?redirect_to='.url()->current()) }}" class="btn btn-primary btn-round">
+                        <i class="material-icons">add</i> Añadir al carrito de compras
+                    </a>
+                @endif
             </div>
 
             <div class="row">
