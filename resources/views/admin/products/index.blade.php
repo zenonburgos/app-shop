4dacd3204316a3bfb22351842a9bdf4401272c0d
unknown <znburgos@gmail.com>
2020-02-15 19:21:39 -0600

    Busqueda en productos admin

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -4,6 +4,65 @@
 
 @section('body-class', 'product-page')
 
+@section('styles')
+    <style>
+        .team .row .col-md-4 {
+            margin-bottom: 5em;
+        }
+        .team .row {
+          display: -webkit-box;
+          display: -webkit-flex;
+          display: -ms-flexbox;
+          display:         flex;
+          flex-wrap: wrap;
+        }
+        .team .row > [class*='col-'] {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .tt-query {
+          -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+             -moz-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+                  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+        }
+
+        .tt-hint {
+          color: #999
+        }
+
+        .tt-menu {    /* used to be tt-dropdown-menu in older versions */
+          width: 222px;
+          margin-top: 4px;
+          padding: 4px 0;
+          background-color: #fff;
+          border: 1px solid #ccc;
+          border: 1px solid rgba(0, 0, 0, 0.2);
+          -webkit-border-radius: 4px;
+             -moz-border-radius: 4px;
+                  border-radius: 4px;
+          -webkit-box-shadow: 0 5px 10px rgba(0,0,0,.2);
+             -moz-box-shadow: 0 5px 10px rgba(0,0,0,.2);
+                  box-shadow: 0 5px 10px rgba(0,0,0,.2);
+        }
+
+        .tt-suggestion {
+          padding: 3px 20px;
+          line-height: 24px;
+        }
+
+        .tt-suggestion.tt-cursor,.tt-suggestion:hover {
+          color: #fff;
+          background-color: #0097cf;
+
+        }
+
+        .tt-suggestion p {
+          margin: 0;
+        }
+    </style>
+@endsection
+
 @section('content')
 <div class="header header-filter" style="background-image: url('https://images.unsplash.com/photo-1423655156442-ccc11daa4e99?crop=entropy&dpr=2&fit=crop&fm=jpg&h=750&ixjsv=2.1.0&ixlib=rb-0.3.5&q=50&w=1450');">
   
@@ -13,6 +72,13 @@
     <div class="container">
         <div class="section text-center">
             <h2 class="title">Listado de productos</h2>
+
+             <form class="form-inline" method="get" action="{{ url('/search') }}">
+                <input type="text" placeholder="¿Qué producto buscas?" class="form-control" name="query" id="search">
+                <button class="btn btn-primary btn-just-icon" type="submit">
+                    <i class="material-icons">search</i>
+                </button>
+            </form>
 
             <div class="team">
                 <div class="row">
@@ -77,3 +143,29 @@
 @include('includes.footer')
 
 @endsection
+
+@section('scripts')
+    <script src="{{ asset('/js/typeahead.bundle.min.js') }}"></script>
+    <script>
+        $(function () {
+            // 
+            var products = new Bloodhound({
+              datumTokenizer: Bloodhound.tokenizers.whitespace,
+              queryTokenizer: Bloodhound.tokenizers.whitespace,
+              prefetch: '{{ url("/products/json") }}'
+              //local: ['hola', 'prueba1', 'prueba2', 'hola2', 'abcde']
+            });            
+
+            // inicializar typeahead sobre nuestro input de búsqueda
+            $('#search').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            }, {
+                name: 'products',
+                source: products
+            });
+        });
+    </script>
+@endsection
+
